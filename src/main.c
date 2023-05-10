b591d548ecb9749eacde2b5f48fbe15a06e91aa9
pdxalz <pdxalz@users.noreply.github.com>
2023-05-09 20:00:52 -0700

    Initial revision.  Windspeed sensor connected to GPIO-10, reporting to Log.

---
+++ src/main.c
@@ -0,0 +1,238 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/net/socket.h>
+#include <zephyr/logging/log.h>
+#include <dk_buttons_and_leds.h>
+#include <modem/lte_lc.h>
+#include <zephyr/drivers/gpio.h>
+
+/* STEP 2.3 - Include the header file for the MQTT Library*/
+#include <zephyr/net/mqtt.h>
+
+#include "mqtt_connection.h"
+/* The mqtt client struct */
+static struct mqtt_client client;
+/* File descriptor */
+static struct pollfd fds;
+
+static K_SEM_DEFINE(lte_connected, 0, 1);
+
+LOG_MODULE_REGISTER(Lesson4_Exercise1, LOG_LEVEL_INF);
+
+#define WIND_SPEED_NODE DT_ALIAS(windspeed0)
+static const struct gpio_dt_spec windspeed = GPIO_DT_SPEC_GET(WIND_SPEED_NODE, gpios);
+
+uint8_t msg[] = "0 hello test";
+uint8_t cnt = 0;
+
+volatile int frequency = 0;
+volatile int64_t lasttime = 0;
+volatile bool toggle = false;
+#define WIND_SCALE (102.0/60.0)
+
+void sample_function(struct k_work *work)
+{
+	float f = frequency / 6.0 * WIND_SCALE;
+	int speed = (int) f;
+	LOG_INF("Wind Speed %d ...", speed);
+	frequency = 0;
+}
+
+K_TIMER_DEFINE(sample_timer, sample_function, NULL);
+
+void windspeed_handler(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+	int64_t time = k_uptime_get();
+	if ((time - lasttime) > 10)
+	{
+		frequency++;
+		toggle = !toggle;
+	}
+	lasttime = time;
+
+	dk_set_led_on(DK_LED3);
+}
+
+static struct gpio_callback windspeed_cb_data;
+
+static void lte_handler(const struct lte_lc_evt *const evt)
+{
+	switch (evt->type)
+	{
+	case LTE_LC_EVT_NW_REG_STATUS:
+		if ((evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_HOME) &&
+			(evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_ROAMING))
+		{
+			break;
+		}
+		LOG_INF("Network registration status: %s",
+				evt->nw_reg_status == LTE_LC_NW_REG_REGISTERED_HOME ? "Connected - home network" : "Connected - roaming");
+		k_sem_give(&lte_connected);
+		break;
+	case LTE_LC_EVT_RRC_UPDATE:
+		LOG_INF("RRC mode: %s", evt->rrc_mode == LTE_LC_RRC_MODE_CONNECTED ? "Connected" : "Idle");
+		break;
+	default:
+		break;
+	}
+}
+
+static void modem_configure(void)
+{
+	LOG_INF("Connecting to LTE network");
+
+	int err = lte_lc_init_and_connect_async(lte_handler);
+	if (err)
+	{
+		LOG_INF("Modem could not be configured, error: %d", err);
+		return;
+	}
+	k_sem_take(&lte_connected, K_FOREVER);
+	LOG_INF("Connected to LTE network");
+	dk_set_led_on(DK_LED2);
+}
+
+static void button_handler(uint32_t button_state, uint32_t has_changed)
+{
+	switch (has_changed)
+	{
+	case DK_BTN1_MSK:
+		/* STEP 7.2 - When button 1 is pressed, call data_publish() to publish a message */
+		if (button_state & DK_BTN1_MSK)
+		{
+			msg[0] = '0' + cnt;
+			cnt = (cnt + 1) % 10;
+			// int err = data_publish(&client, MQTT_QOS_1_AT_LEAST_ONCE,
+			// 					   CONFIG_BUTTON_EVENT_PUBLISH_MSG, sizeof(CONFIG_BUTTON_EVENT_PUBLISH_MSG) - 1);
+			int err = data_publish(&client, MQTT_QOS_1_AT_LEAST_ONCE,
+								   msg, sizeof(msg) - 1);
+			if (err)
+			{
+				LOG_INF("Failed to send message, %d", err);
+				return;
+			}
+		}
+		break;
+	}
+}
+
+void main(void)
+{
+	int err;
+	uint32_t connect_attempt = 0;
+
+	if (dk_leds_init() != 0)
+	{
+		LOG_ERR("Failed to initialize the LED library");
+	}
+
+	if (!device_is_ready(windspeed.port))
+	{
+		return;
+	}
+
+	err = gpio_pin_configure_dt(&windspeed, GPIO_INPUT | GPIO_PULL_UP);
+	if (err < 0)
+	{
+		return;
+	}
+	/* STEP 3 - Configure the interrupt on the button's pin */
+	err = gpio_pin_interrupt_configure_dt(&windspeed, GPIO_INT_EDGE_TO_ACTIVE);
+
+	/* STEP 6 - Initialize the static struct gpio_callback variable   */
+	gpio_init_callback(&windspeed_cb_data, windspeed_handler, BIT(windspeed.pin));
+
+	/* STEP 7 - Add the callback function by calling gpio_add_callback()   */
+	gpio_add_callback(windspeed.port, &windspeed_cb_data);
+
+	modem_configure();
+
+	if (dk_buttons_init(button_handler) != 0)
+	{
+		LOG_ERR("Failed to initialize the buttons library");
+	}
+
+	err = client_init(&client);
+	if (err)
+	{
+		LOG_ERR("Failed to initialize MQTT client: %d", err);
+		return;
+	}
+
+	k_timer_start(&sample_timer, K_SECONDS(6), K_SECONDS(6));
+
+do_connect:
+	if (connect_attempt++ > 0)
+	{
+		LOG_INF("Reconnecting in %d seconds...",
+				CONFIG_MQTT_RECONNECT_DELAY_S);
+		k_sleep(K_SECONDS(CONFIG_MQTT_RECONNECT_DELAY_S));
+	}
+	err = mqtt_connect(&client);
+	if (err)
+	{
+		LOG_ERR("Error in mqtt_connect: %d", err);
+		goto do_connect;
+	}
+
+	err = fds_init(&client, &fds);
+	if (err)
+	{
+		LOG_ERR("Error in fds_init: %d", err);
+		return;
+	}
+
+	while (1)
+	{
+		err = poll(&fds, 1, mqtt_keepalive_time_left(&client));
+		if (err < 0)
+		{
+			LOG_ERR("Error in poll(): %d", errno);
+			break;
+		}
+
+		err = mqtt_live(&client);
+		if ((err != 0) && (err != -EAGAIN))
+		{
+			LOG_ERR("Error in mqtt_live: %d", err);
+			break;
+		}
+
+		if ((fds.revents & POLLIN) == POLLIN)
+		{
+			err = mqtt_input(&client);
+			if (err != 0)
+			{
+				LOG_ERR("Error in mqtt_input: %d", err);
+				break;
+			}
+		}
+
+		if ((fds.revents & POLLERR) == POLLERR)
+		{
+			LOG_ERR("POLLERR");
+			break;
+		}
+
+		if ((fds.revents & POLLNVAL) == POLLNVAL)
+		{
+			LOG_ERR("POLLNVAL");
+			break;
+		}
+	}
+
+	LOG_INF("Disconnecting MQTT client");
+
+	err = mqtt_disconnect(&client);
+	if (err)
+	{
+		LOG_ERR("Could not disconnect MQTT client: %d", err);
+	}
+	goto do_connect;
+}
